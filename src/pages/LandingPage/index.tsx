2138adb58b0645e5ff19392c44fcdedc62f1a6e6
igorWebMaster <igor_1917@hotmail.com>
2022-01-24 17:32:27 -0300

    chore: add start page and styles

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-
+import  Button from 'components/Button'
 import * as Styled  from './styles'
+
+import pokemonLogo from 'assets/images/pokemonLogo.png'
+import { Link } from 'react-router-dom';
 
 export  function LandingPage() {
   return (
     <Styled.ContainerHomeScreen>
-        <button>
-            START
-        </button>
+        <Styled.ContentHome>
+            <Styled.Logo>
+                <img src={pokemonLogo} alt="" />
+            </Styled.Logo>
+            <Styled.HomeContent>
+                <Link to="/game">
+                    <Button
+                        text={'START'}
+                        icon=""
+                        onClick={() => {}}
+                        onlyIcon="false"
+                 />
+               </Link>
+            </Styled.HomeContent>
+        </Styled.ContentHome>
     </Styled.ContainerHomeScreen>
   )
 }
--- src/pages/LandingPage/styles.ts
+++ src/pages/LandingPage/styles.ts
@@ -1,10 +1,26 @@
 import styled from 'styled-components';
 
 export const ContainerHomeScreen = styled.div`
-]   height: 100vh;
-    width: 100vh;
-    background: url(${require('../../assets/images/background.png')}) no-repeat center center fixed;
+    height: 100vh;
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
+    background-image: linear-gradient(to right, #45e97b , #38f9d5);
+
+`;
+
+export const ContentHome = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+export const Logo = styled.div`
+  margin: 2rem 0;  
+`;
+
+export const HomeContent = styled.div`
+    
 `;
