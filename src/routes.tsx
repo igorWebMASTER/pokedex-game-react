0a15ba3a13ed52823c07fa4d603c743fd6580b49
igorWebMaster <igor_1917@hotmail.com>
2022-02-05 23:45:04 -0300

    chore: add pokemon provider to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,18 +6,21 @@ import Layout from './components/Layout';
 
 import React from 'react';
 import { Toaster } from 'react-hot-toast';
+import { PokemonProvider } from 'context/pokemonContext';
 
 const Routes = (): JSX.Element => {
     return (
         <BrowserRouter>
             <Switch>
               <ManageUIContext>
-               <Layout>
-                  <Route exact component={InitialPage}  path="/" />
-                  <Route exact component={Map}  path="/game" />
-                  <Route render={() => <Redirect to="/" />} />
-                  <Toaster/>
-                </Layout>
+               <PokemonProvider>
+                  <Layout>
+                    <Route exact component={InitialPage}  path="/" />
+                    <Route exact component={Map}  path="/game" />
+                    <Route render={() => <Redirect to="/" />} />
+                    <Toaster/>
+                  </Layout>
+                </PokemonProvider>
               </ManageUIContext>
             </Switch>
         </BrowserRouter>
