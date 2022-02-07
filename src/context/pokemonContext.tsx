b4c67d209ade4f5825ac0db6785a344e8f96929b
igorWebMaster <igor_1917@hotmail.com>
2022-02-06 21:32:01 -0300

    chore: add display name to pokemon context

--- src/context/pokemonContext.tsx
+++ src/context/pokemonContext.tsx
@@ -1,6 +1,5 @@
 import { getPokemonById } from 'app/api';
-import { PokemonProps } from 'dtos/pokemon';
-import { createContext, ReactNode, useState, useCallback, useEffect } from 'react';
+import { createContext, ReactNode, useState } from 'react';
 import toast from 'react-hot-toast';
 import { generatePokemonId } from 'utils/generateRandomPokemonID';
 
@@ -16,21 +15,24 @@ interface PokemonProviderProps {
 
 export const PokemonContext = createContext({} as PokemonContextData);
 
+PokemonContext.displayName = 'PokemonContext';
+
 export function PokemonProvider({ children }: PokemonProviderProps) {
     const [isLoading, setIsLoading] = useState(false);
     const [randomPokemonData, setRandomPokemonData] = useState(
       {} 
     );
-  
+
     async function handleGetRandomPokemon() {
       try {
         setRandomPokemonData([])
         setIsLoading(true);
         const pokemonId = generatePokemonId();
-        const pokemonData = await getPokemonById(pokemonId)
-         setRandomPokemonData(pokemonData);
-         setIsLoading(false);
-
+        const pokemonData = await getPokemonById(pokemonId);
+        setTimeout(() =>{
+          setRandomPokemonData(pokemonData); 
+          setIsLoading(false);
+        }, 300);
       } catch (e: any ) {
         toast.error('Não foi possível gerar o pokemon neste momento. tente novamente mais tarde!')
       } 
