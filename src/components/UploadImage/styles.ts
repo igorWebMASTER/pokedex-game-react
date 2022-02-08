bfea9b0dea74e4233afbdc7e1d97e0fbf9ce138d
igorWebMaster <igor_1917@hotmail.com>
2022-02-07 23:02:10 -0300

    chore: add upload image component

---
+++ src/components/UploadImage/styles.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const UploadImageContainer = styled.div`
+    .upload-image-container {
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+
+    button{
+        border:0;
+        background:transparent;
+        display: flex;
+        
+        cursor: pointer;
+    }
+
+    .image-item__btn-wrapper{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin:1rem 3rem;
+    }
+
+`
