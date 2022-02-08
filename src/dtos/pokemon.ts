b9cbfeccb054a4cbdb49be99f76d52ee947a34ae
igorWebMaster <igor_1917@hotmail.com>
2022-02-07 23:05:54 -0300

    chore: adjustment on tooltip

--- src/dtos/pokemon.ts
+++ src/dtos/pokemon.ts
@@ -2,11 +2,12 @@ export interface PokemonProps {
   [x: string]: any;
   lenght: number;
   name: string;
-  id: string;
+  id?: string | undefined;
   weight: number;
   base_experience: number;
   hp: number;
   attack?: string;
+  isSelected?: boolean;
   specialAttack?: string;
   specialDefense?: string;
   defense?: string;
